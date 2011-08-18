5a65d8b6ee6bef9b4063f1be632199ec94e3a3de
Jon Fleschler <jon.fleschler@gmail.com>
2011-08-18 16:41:23 -0500

    Done with user edit/update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -3,10 +3,12 @@ source 'http://rubygems.org'
 gem 'rails'
 gem 'sqlite3'
 gem 'gravatar_image_tag'
+gem 'will_paginate'
 
 group :development do
    gem 'rspec-rails'
    gem 'annotate'
+   gem 'faker'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110817202027) do
+ActiveRecord::Schema.define(:version => 20110818212950) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20110817202027) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
